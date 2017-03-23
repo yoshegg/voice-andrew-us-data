8926af0fa9db39fb5c8eb756e663cb2d64d266b8
Christophe Biwer <christophe.biwer@dfki.de>
2017-03-23 14:06:58 +0100

    cleaned up

--- yamlRetrieveAndParse/split_flac.py
+++ yamlRetrieveAndParse/split_flac.py
@@ -12,11 +12,15 @@ with open(ymlGrid_file, 'r') as ymlGrid:
     except yaml.YAMLError as exc:
         print(exc)
 
+os.system("rm -rf target/")
+os.system("mkdir target/")
+
 for x in library.items():
+  target = "target/"
   filename = x[0] + ".wav"
   sta = str(x[1]["sta"])
   end = " =" + str(x[1]["end"])
-  command = "sox " +  flac_file + " " +  filename + " trim " + sta + end + " rate 16000"
+  command = "sox " +  flac_file + " " +  target + filename + " trim " + sta + end + " rate 16000"
   print("execute: " + command)
   os.system(command)
   print(" > saved " + filename + "\n")
