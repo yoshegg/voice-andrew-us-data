3035f5fd79ccc27b2744c8c7bc4ab5108169000b
Christophe Biwer <christophe.biwer@dfki.de>
2017-03-29 10:32:33 +0200

    corrected script

--- retrieveWavFiles.sh
+++ retrieveWavFiles.sh
@@ -1,2 +1,2 @@
 curl -LOk https://github.com/yoshegg/voice-andrew-us-data/releases/download/audio_normalized/normalized_wavs.zip
-unzip wav.zip -d resources/
+unzip normalized_wavs.zip -d resources/
